022130c20783a94a46f956b90c547ad29a6127e3
Diego Mariano <diogohenks@hotmail.com>
2022-06-18 13:31:09 -0300

    Update web.php

--- web/routes/web.php
+++ web/routes/web.php
@@ -20,3 +20,5 @@ Route::get('/febreamarela', [DashboardController::class, 'febreamaerela']);
 Route::get('/dengue', [DashboardController::class, 'dengue']);
 Route::get('/zika', [DashboardController::class, 'zika']);
 Route::get('/hiv', [DashboardController::class, 'hiv']);
+
+Route::get('/video_institucional', fn()=>redirect('https://www.canva.com/design/DAFD9QOzquQ/VjPKdo-x6W__UfFbb2ZzEg/watch?utm_content=DAFD9QOzquQ&utm_campaign=share_your_design&utm_medium=link&utm_source=shareyourdesignpanel')); 
