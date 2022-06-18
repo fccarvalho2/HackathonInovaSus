652ca76ca6d068ea298345ed23c7c587380fa6c0
Diego Mariano <diogohenks@hotmail.com>
2022-06-17 22:48:55 -0300

    Update dashboard.blade.php

--- web/resources/views/dashboard.blade.php
+++ web/resources/views/dashboard.blade.php
@@ -22,11 +22,11 @@
       <div class="card shadow" style="border-left:5px #007bb1 solid">
         <div class="card-body">
           <div class="row">
-            <div class="col-6 text-center">
+            <div class="col-md-6 col-12 text-center">
               <h5 class="card-title display-4" style="color:#007bb1"><i class="bi bi-file-earmark-text-fill"></i></h5>
               <h6 class="card-subtitle mb-2" style="color:#007bb1"><b>Artigos</b></h6>
             </div>
-            <div class="col-6">
+            <div class="col-md-6 col-12">
               <p class="display-5 pt-3">2147</p>
             </div>
           </div>
@@ -39,11 +39,11 @@
       <div class="card shadow" style="border-left:5px #ff7925 solid">
         <div class="card-body">
           <div class="row">
-            <div class="col-6 text-center">
+            <div class="col-md-6 col-12 text-center">
               <h5 class="card-title display-4" style="color:#ff7925"><i class="bi bi-award-fill"></i></h5>
               <h6 class="card-subtitle mb-2" style="color:#ff7925"><b>Qualis A</b></h6>
             </div>
-            <div class="col-6">
+            <div class="col-md-6 col-12">
               <p class="display-5 pt-3">652</p>
             </div>
           </div>
@@ -56,11 +56,11 @@
       <div class="card shadow" style="border-left:5px #009e39 solid">
         <div class="card-body">
           <div class="row">
-            <div class="col-6 text-center">
+            <div class="col-md-6 col-12 text-center">
               <h5 class="card-title display-4" style="color:#009e39"><i class="bi bi-motherboard-fill"></i></h5>
               <h6 class="card-subtitle mb-2" style="color:#009e39"><b>Tecnologias</b></h6>
             </div>
-            <div class="col-6">
+            <div class="col-md-6 col-12">
               <p class="display-5 pt-3">564</p>
             </div>
           </div>
@@ -73,11 +73,11 @@
       <div class="card shadow" style="border-left:5px #e21a2a solid">
         <div class="card-body">
           <div class="row">
-            <div class="col-6 text-center">
+            <div class="col-md-6 col-12 text-center">
               <h5 class="card-title display-4" style="color:#e21a2a"><i class="bi bi-chat-square-quote-fill"></i></h5>
               <h6 class="card-subtitle mb-2" style="color:#e21a2a"><b>PLN</b></h6>
             </div>
-            <div class="col-6">
+            <div class="col-md-6 col-12">
               <p class="display-5 pt-3">72</p>
             </div>
           </div>
@@ -88,55 +88,55 @@
 <div class="row mt-5">
 
   <!-- grafico -->
-  <div class="col-6 my-2 col-xxl-6">
+  <div class="col-12 my-2 col-xl-6">
     <h4>Publicações por data  (n = 2147)</h4>
     <canvas class="my-4 w-100" id="g1" width="900" height="450"></canvas>
   </div>
 
  <!-- grafico -->
- <div class="col-6 my-2 col-xxl-6">
+ <div class="col-12 my-2 col-xl-6">
     <h4>Relevância das publicações</h4>
     <canvas class="my-4 w-100" id="g15" width="900" height="450"></canvas>
   </div>
 
   <!-- grafico -->
-  <div class="col-6 my-2 col-xxl-6">
+  <div class="col-12 my-2 col-xl-6">
     <h4>Publicações por Qualis (652/2147)</h4>
     <canvas class="my-4 w-100" id="g2" width="900" height="450"></canvas>
   </div>
 
   <!-- grafico -->
-  <div class="col-6 my-2 col-xxl-6">
+  <div class="col-6 my-2 col-xl-6">
     <h4>Publicações por fator de impacto (1145/2147)</h4>
     <canvas class="my-4 w-100" id="g3" width="900" height="450"></canvas>
   </div>
 
   <!-- grafico -->
-  <div class="col-6 my-2 col-xxl-6">
+  <div class="col-6 my-2 col-xl-6">
     <h4>Publicações por média de citações (últimos 3 anos)</h4>
     <canvas class="my-4 w-100" id="g35" width="900" height="450"></canvas>
   </div>
 
   <!-- grafico -->
-  <div class="col-4 my-6 col-xxl-6">
+  <div class="col-12 my-2 col-xl-6">
     <h4>Publicações por h-index (1145/2147)</h4>
     <canvas class="my-4 w-100" id="g4" width="900" height="450"></canvas>
   </div>
 
   <!-- grafico -->
-  <div class="col-6 my-2 col-xxl-6">
+  <div class="col-12 my-2 col-xl-6">
     <h4>Tecnologias mais citadas (20)</h4>
     <canvas class="my-4 w-100" id="g5" width="900" height="450"></canvas>
   </div>
 
   <!-- grafico -->
-  <div class="col-6 my-2 col-xxl-6">
+  <div class="col-12 my-2 col-xxl-6">
     <h4>Tecnologias em artigos com impacto (20)</h4>
     <canvas class="my-4 w-100" id="g6" width="900" height="450"></canvas>
   </div>
 
   <!-- grafico -->
-  <div class="col-12 my-2 col-xxl-12">
+  <div class="col-12 my-2 col-xl-12">
     <h4>Análise do processamento de linguagem natural (72)</h4>
     <p>
       <span style="background:#ffa500" class="p-1 px-3 m-2 rounded"> </span> Tendência desfavorável/neutro
